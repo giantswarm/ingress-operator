f29fe7f55e67a7becfeb37787efc1d77049f2413
Tim Schindler <xh3b4sd@gmail.com>
2017-06-12 11:58:08 +0200

    added basic microservice structure

---
+++ service/service.go
@@ -0,0 +1,113 @@
+// Package service implements business logic to create Kubernetes resources
+// against the Kubernetes API.
+package service
+
+import (
+	"fmt"
+	"sync"
+
+	microerror "github.com/giantswarm/microkit/error"
+	micrologger "github.com/giantswarm/microkit/logger"
+	"github.com/spf13/viper"
+
+	"github.com/giantswarm/ingress-operator/flag"
+	"github.com/giantswarm/ingress-operator/service/operator"
+	"github.com/giantswarm/ingress-operator/service/version"
+)
+
+// Config represents the configuration used to create a new service.
+type Config struct {
+	// Dependencies.
+	Logger micrologger.Logger
+
+	// Settings.
+	Flag  *flag.Flag
+	Viper *viper.Viper
+
+	Description string
+	GitCommit   string
+	Name        string
+	Source      string
+}
+
+// DefaultConfig provides a default configuration to create a new service by
+// best effort.
+func DefaultConfig() Config {
+	return Config{
+		// Dependencies.
+		Logger: nil,
+
+		// Settings.
+		Flag:  nil,
+		Viper: nil,
+
+		Description: "",
+		GitCommit:   "",
+		Name:        "",
+		Source:      "",
+	}
+}
+
+// New creates a new configured service object.
+func New(config Config) (*Service, error) {
+	// Dependencies.
+	if config.Logger == nil {
+		return nil, microerror.MaskAnyf(invalidConfigError, "logger must not be empty")
+	}
+	config.Logger.Log("debug", fmt.Sprintf("creating kvm-operator with config: %#v", config))
+
+	var err error
+
+	var operatorService *operator.Service
+	{
+		operatorConfig := operator.DefaultConfig()
+
+		operatorConfig.Logger = config.Logger
+
+		operatorService, err = operator.New(operatorConfig)
+		if err != nil {
+			return nil, microerror.MaskAny(err)
+		}
+	}
+
+	var versionService *version.Service
+	{
+		versionConfig := version.DefaultConfig()
+
+		versionConfig.Description = config.Description
+		versionConfig.GitCommit = config.GitCommit
+		versionConfig.Name = config.Name
+		versionConfig.Source = config.Source
+
+		versionService, err = version.New(versionConfig)
+		if err != nil {
+			return nil, microerror.MaskAny(err)
+		}
+	}
+
+	newService := &Service{
+		// Dependencies.
+		Operator: operatorService,
+		Version:  versionService,
+
+		// Internals
+		bootOnce: sync.Once{},
+	}
+
+	return newService, nil
+}
+
+type Service struct {
+	// Dependencies.
+	Operator *operator.Service
+	Version  *version.Service
+
+	// Internals.
+	bootOnce sync.Once
+}
+
+func (s *Service) Boot() {
+	s.bootOnce.Do(func() {
+		s.Operator.Boot()
+	})
+}
