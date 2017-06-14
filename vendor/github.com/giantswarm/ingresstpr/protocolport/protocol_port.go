c67a5bb4d231e8f26ea1ceb223a88c283bea7969
Tim Schindler <xh3b4sd@gmail.com>
2017-06-14 17:51:19 +0200

    updated vendor deps

--- vendor/github.com/giantswarm/ingresstpr/protocolport/protocol_port.go
+++ vendor/github.com/giantswarm/ingresstpr/protocolport/protocol_port.go
@@ -1,7 +1,7 @@
 package protocolport
 
 type ProtocolPort struct {
-	IngressPort string `json:"ingressPort" yaml:"ingressPort"`
-	LBPort      string `json:"lbPort" yaml:"lbPort"`
+	IngressPort int    `json:"ingressPort" yaml:"ingressPort"`
+	LBPort      int    `json:"lbPort" yaml:"lbPort"`
 	Protocol    string `json:"protocol" yaml:"protocol"`
 }
