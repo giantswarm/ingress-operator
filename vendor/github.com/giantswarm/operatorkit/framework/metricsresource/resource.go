8d9706b75ebbb19978b1452299e025db3e593dd9
Tim Schindler <xh3b4sd@gmail.com>
2017-10-06 16:30:57 +0200

    issue metrics for error rates for testing

--- vendor/github.com/giantswarm/operatorkit/framework/metricsresource/resource.go
+++ vendor/github.com/giantswarm/operatorkit/framework/metricsresource/resource.go
@@ -102,6 +102,7 @@ func (r *Resource) GetCreateState(ctx context.Context, obj, currentState, desire
 	o := "GetCreateState"
 
 	defer r.updateMetrics(o, time.Now())
+	r.updateErrorMetrics(o)
 
 	v, err := r.resource.GetCreateState(ctx, obj, currentState, desiredState)
 	if err != nil {
@@ -116,7 +117,6 @@ func (r *Resource) GetDeleteState(ctx context.Context, obj, currentState, desire
 	o := "GetDeleteState"
 
 	defer r.updateMetrics(o, time.Now())
-	r.updateErrorMetrics(o)
 
 	v, err := r.resource.GetDeleteState(ctx, obj, currentState, desiredState)
 	if err != nil {
