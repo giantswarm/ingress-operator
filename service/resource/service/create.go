57f270af6af8b3bfdc40393f55f9f6b50f8e5da7
Tim Schindler <xh3b4sd@gmail.com>
2017-11-20 10:54:14 +0100

    moved update logic to update event processing

--- service/resource/service/create.go
+++ service/resource/service/create.go
@@ -2,77 +2,11 @@ package service
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/giantswarm/microerror"
-	apiv1 "k8s.io/client-go/pkg/api/v1"
 )
 
+// ApplyCreateChange is a no-op. The ingress-operator manages a service that
+// already exists. Thus only update proceedures are done. The creation of the
+// service being maintained is ensured by another component.
 func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange interface{}) error {
-	customObject, err := toCustomObject(obj)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-	serviceToCreate, err := toService(createChange)
-	if err != nil {
-		return microerror.Mask(err)
-	}
-
-	if serviceToCreate != nil {
-		r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "creating the service data in the Kubernetes API")
-
-		namespace := customObject.Spec.HostCluster.IngressController.Namespace
-		_, err := r.k8sClient.CoreV1().Services(namespace).Update(serviceToCreate)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "created the service data in the Kubernetes API")
-	} else {
-		r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "the service data does not need to be created in the Kubernetes API")
-	}
-
 	return nil
-}
-
-func (r *Resource) newCreateChange(ctx context.Context, obj, currentState, desiredState interface{}) (interface{}, error) {
-	customObject, err := toCustomObject(obj)
-	if err != nil {
-		return microerror.Mask(err), nil
-	}
-	currentService, err := toService(currentState)
-	if err != nil {
-		return microerror.Mask(err), nil
-	}
-	dState, ok := desiredState.([]apiv1.ServicePort)
-	if !ok {
-		return nil, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", []apiv1.ServicePort{}, desiredState)
-	}
-
-	r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "get create state", "resource", "service")
-
-	// Make sure the current state of the Kubernetes resources is known by the
-	// create action. The resources we already fetched represent the source of
-	// truth. They have to be used as base to actually update the resources in the
-	// next steps.
-	createState := currentService
-
-	// Find anything which is in desired state but not in the current state. This
-	// lets us drive the current state towards the desired state, because
-	// everything we find here is supposed to be created. Note that the creation
-	// of config-map data and service ports is always only an update operation
-	// against the Kubernetes API. Anyway, this concept here implements how a real
-	// reconciliation should drive specific parts of the current state towards the
-	// desired state, because a decent reconciliation is not always only an update
-	// operation of existing resources, but e.g. creation of resources. In our
-	// case here we only transform data within resources. Therefore the update.
-	for _, p := range dState {
-		if !inServicePorts(createState.Spec.Ports, p) {
-			createState.Spec.Ports = append(createState.Spec.Ports, p)
-		}
-	}
-
-	r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", fmt.Sprintf("found create state: %#v", createState), "resource", "service")
-
-	return createState, nil
 }
