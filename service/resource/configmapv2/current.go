99f31185c05cfd507cece89ee59316deff74752a
Tim Schindler <xh3b4sd@gmail.com>
2017-11-30 15:18:34 +0100

    debug

--- service/resource/configmapv2/current.go
+++ service/resource/configmapv2/current.go
@@ -2,37 +2,34 @@ package configmapv2
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/giantswarm/microerror"
-	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
 	r.logger.LogCtx(ctx, "info", "v2 resource executed")
 	return nil, nil
+	/*
+		customObject, err := toCustomObject(obj)
+		if err != nil {
+			return microerror.Mask(err), nil
+		}
 
-	customObject, err := toCustomObject(obj)
-	if err != nil {
-		return microerror.Mask(err), nil
-	}
+		r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "get current state")
 
-	r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "get current state")
+		// Lookup the current state of the configmap.
+		namespace := customObject.Spec.HostCluster.IngressController.Namespace
+		configMap := customObject.Spec.HostCluster.IngressController.ConfigMap
+		k8sConfigMap, err := r.k8sClient.CoreV1().ConfigMaps(namespace).Get(configMap, apismetav1.GetOptions{})
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+		// Ensure that the map is assignable. This prevents panics down the road in
+		// case the config map has no data at all.
+		if k8sConfigMap.Data == nil {
+			k8sConfigMap.Data = map[string]string{}
+		}
 
-	// Lookup the current state of the configmap.
-	namespace := customObject.Spec.HostCluster.IngressController.Namespace
-	configMap := customObject.Spec.HostCluster.IngressController.ConfigMap
-	k8sConfigMap, err := r.k8sClient.CoreV1().ConfigMaps(namespace).Get(configMap, apismetav1.GetOptions{})
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
-	// Ensure that the map is assignable. This prevents panics down the road in
-	// case the config map has no data at all.
-	if k8sConfigMap.Data == nil {
-		k8sConfigMap.Data = map[string]string{}
-	}
+		r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", fmt.Sprintf("found k8s state: %#v", *k8sConfigMap))
 
-	r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", fmt.Sprintf("found k8s state: %#v", *k8sConfigMap))
-
-	return k8sConfigMap, nil
+		return k8sConfigMap, nil
+	*/
 }
--- service/resource/servicev2/current.go
+++ service/resource/servicev2/current.go
@@ -2,31 +2,28 @@ package servicev2
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/giantswarm/microerror"
-	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
 	r.logger.LogCtx(ctx, "info", "v2 resource executed")
 	return nil, nil
+	/*
+		customObject, err := toCustomObject(obj)
+		if err != nil {
+			return microerror.Mask(err), nil
+		}
 
-	customObject, err := toCustomObject(obj)
-	if err != nil {
-		return microerror.Mask(err), nil
-	}
+		r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "get current state")
 
-	r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", "get current state")
+		namespace := customObject.Spec.HostCluster.IngressController.Namespace
+		service := customObject.Spec.HostCluster.IngressController.Service
+		k8sService, err := r.k8sClient.CoreV1().Services(namespace).Get(service, apismetav1.GetOptions{})
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
 
-	namespace := customObject.Spec.HostCluster.IngressController.Namespace
-	service := customObject.Spec.HostCluster.IngressController.Service
-	k8sService, err := r.k8sClient.CoreV1().Services(namespace).Get(service, apismetav1.GetOptions{})
-	if err != nil {
-		return nil, microerror.Mask(err)
-	}
+		r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", fmt.Sprintf("found k8s state: %#v", *k8sService))
 
-	r.logger.Log("cluster", customObject.Spec.GuestCluster.ID, "debug", fmt.Sprintf("found k8s state: %#v", *k8sService))
-
-	return k8sService, nil
+		return k8sService, nil
+	*/
 }
