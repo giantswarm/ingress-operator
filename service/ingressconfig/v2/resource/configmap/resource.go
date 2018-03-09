0d95ab16e6f7d2185e4562b7aa5b31a7fca0aa2d
Tim Schindler <xh3b4sd@gmail.com>
2018-03-09 17:05:45 +0100

    applies several operatorkit changes

--- service/ingressconfig/v2/resource/configmap/resource.go
+++ service/ingressconfig/v2/resource/configmap/resource.go
@@ -3,7 +3,6 @@ package configmap
 import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
-	"github.com/giantswarm/operatorkit/framework"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 
@@ -69,10 +68,6 @@ func New(config Config) (*Resource, error) {
 
 func (r *Resource) Name() string {
 	return Name
-}
-
-func (r *Resource) Underlying() framework.Resource {
-	return r
 }
 
 func inConfigMapData(data map[string]string, k, v string) bool {
