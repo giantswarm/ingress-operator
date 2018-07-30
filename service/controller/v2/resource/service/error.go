78978a5cda9786fb5fb28e767ccad9d82f8631be
Tim Schindler <xh3b4sd@gmail.com>
2018-07-30 15:47:41 +0200

    rewrite errors (#99)

--- service/controller/v2/resource/service/error.go
+++ service/controller/v2/resource/service/error.go
@@ -4,21 +4,27 @@ import (
 	"github.com/giantswarm/microerror"
 )
 
-var invalidConfigError = microerror.New("invalid config")
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
-var servicePortNotFoundError = microerror.New("service port not found")
+var servicePortNotFoundError = &microerror.Error{
+	Kind: "servicePortNotFoundError",
+}
 
 // IsServicePortNotFound asserts servicePortNotFoundError.
 func IsServicePortNotFound(err error) bool {
 	return microerror.Cause(err) == servicePortNotFoundError
 }
 
-var wrongTypeError = microerror.New("wrong type")
+var wrongTypeError = &microerror.Error{
+	Kind: "wrongTypeError",
+}
 
 // IsWrongType asserts wrongTypeError.
 func IsWrongType(err error) bool {
