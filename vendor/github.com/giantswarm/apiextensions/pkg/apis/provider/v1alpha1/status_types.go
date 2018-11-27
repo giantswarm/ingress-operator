43c8f0fd4a89c8f68704bf1beae6239ff6eedddd
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-27 10:13:36 +0000

    vendor: update

--- vendor/github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1/status_types.go
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1/status_types.go
@@ -17,6 +17,11 @@ const (
 )
 
 const (
+	StatusClusterTypeDeleted  = "Deleted"
+	StatusClusterTypeDeleting = "Deleting"
+)
+
+const (
 	StatusClusterTypeUpdated  = "Updated"
 	StatusClusterTypeUpdating = "Updating"
 )
@@ -30,6 +35,9 @@ type StatusCluster struct {
 	// Nodes is a list of guest cluster node information reflecting the current
 	// state of the guest cluster nodes.
 	Nodes []StatusClusterNode `json:"nodes" yaml:"nodes"`
+	// Resources is a list of arbitrary conditions of operatorkit resource
+	// implementations.
+	Resources []StatusClusterResource `json:"resources" yaml:"resources"`
 	// Versions is a list that acts like a historical track record of versions a
 	// guest cluster went through. A version is only added to the list as soon as
 	// the guest cluster successfully migrated to the version added here.
@@ -39,10 +47,15 @@ type StatusCluster struct {
 // StatusClusterCondition expresses the conditions in which a guest cluster may
 // is.
 type StatusClusterCondition struct {
+	// LastHeartbeatTime is the last time we got an update on a given condition.
+	LastHeartbeatTime DeepCopyTime `json:"lastHeartbeatTime" yaml:"lastHeartbeatTime"`
+	// LastTransitionTime is the last time the condition transitioned from one
+	// status to another.
+	LastTransitionTime DeepCopyTime `json:"lastTransitionTime" yaml:"lastTransitionTime"`
 	// Status may be True, False or Unknown.
 	Status string `json:"status" yaml:"status"`
 	// Type may be Creating, Created, Scaling, Scaled, Draining, Drained,
-	// Deleting, Deleted.
+	// Updating, Updated, Deleting, Deleted.
 	Type string `json:"type" yaml:"type"`
 }
 
@@ -54,15 +67,57 @@ type StatusClusterNetwork struct {
 
 // StatusClusterNode holds information about a guest cluster node.
 type StatusClusterNode struct {
-	Name    string `json:"name" yaml:"name"`
+	// LastHeartbeatTime is the last time we got an update on a given condition.
+	LastHeartbeatTime DeepCopyTime `json:"lastHeartbeatTime" yaml:"lastHeartbeatTime"`
+	// LastTransitionTime is the last time the condition transitioned from one
+	// status to another.
+	LastTransitionTime DeepCopyTime `json:"lastTransitionTime" yaml:"lastTransitionTime"`
+	// Name referrs to a tenant cluster node name.
+	Name string `json:"name" yaml:"name"`
+	// Version referrs to the version used by the node as mandated by the provider
+	// operator.
 	Version string `json:"version" yaml:"version"`
+}
+
+// Resource is structure holding arbitrary conditions of operatorkit resource
+// implementations. Imagine an operator implements an instance resource. This
+// resource may operates sequentially but has to operate based on a certain
+// system state it manages. So it tracks the status as needed here specific to
+// its own implementation and means in order to fulfil its premise.
+type StatusClusterResource struct {
+	Conditions []StatusClusterResourceCondition `json:"conditions" yaml:"conditions"`
+	Name       string                           `json:"name" yaml:"name"`
+}
+
+// StatusClusterResourceCondition expresses the conditions in which an
+// operatorkit resource may is.
+type StatusClusterResourceCondition struct {
+	// LastHeartbeatTime is the last time we got an update on a given condition.
+	LastHeartbeatTime DeepCopyTime `json:"lastHeartbeatTime" yaml:"lastHeartbeatTime"`
+	// LastTransitionTime is the last time the condition transitioned from one
+	// status to another.
+	LastTransitionTime DeepCopyTime `json:"lastTransitionTime" yaml:"lastTransitionTime"`
+	// Status may be True, False or Unknown.
+	Status string `json:"status" yaml:"status"`
+	// Type may be anything an operatorkit resource may define.
+	Type string `json:"type" yaml:"type"`
 }
 
 // StatusClusterVersion expresses the versions in which a guest cluster was and
 // may still be.
 type StatusClusterVersion struct {
-	// Date is the time of the given guest cluster version being updated.
+	// TODO date is deprecated due to LastTransitionTime and LastHeartbeatTime.
+	// This can be removed ones the new properties are properly used in all tenant
+	// clusters.
+	//
+	//     https://github.com/giantswarm/giantswarm/issues/3988
+	//
 	Date time.Time `json:"date" yaml:"date"`
+	// LastHeartbeatTime is the last time we got an update on a given condition.
+	LastHeartbeatTime DeepCopyTime `json:"lastHeartbeatTime" yaml:"lastHeartbeatTime"`
+	// LastTransitionTime is the last time the condition transitioned from one
+	// status to another.
+	LastTransitionTime DeepCopyTime `json:"lastTransitionTime" yaml:"lastTransitionTime"`
 	// Semver is some semver version, e.g. 1.0.0.
 	Semver string `json:"semver" yaml:"semver"`
 }
--- vendor/github.com/giantswarm/operatorkit/informer/error.go
+++ vendor/github.com/giantswarm/operatorkit/informer/error.go
@@ -2,47 +2,38 @@ package informer
 
 import (
 	"github.com/giantswarm/microerror"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
-var alreadyRegisteredError = microerror.New("already registered")
-
-// IsAlreadyRegisteredError asserts alreadyRegisteredError.
-func IsAlreadyRegisteredError(err error) bool {
-	c := microerror.Cause(err)
-	_, ok := c.(prometheus.AlreadyRegisteredError)
-	if ok {
-		return true
-	}
-	if c == alreadyRegisteredError {
-		return true
-	}
-
-	return false
+var contextCanceledError = &microerror.Error{
+	Kind: "contextCanceledError",
 }
-
-var contextCanceledError = microerror.New("context canceled")
 
 // IsContextCanceled asserts contextCanceledError.
 func IsContextCanceled(err error) bool {
 	return microerror.Cause(err) == contextCanceledError
 }
 
-var initializationTimedOutError = microerror.New("initialization timed out")
+var initializationTimedOutError = &microerror.Error{
+	Kind: "initializationTimedOutError",
+}
 
 // IsInitializationTimedOut asserts initializationTimedOutError.
 func IsInitializationTimedOut(err error) bool {
 	return microerror.Cause(err) == initializationTimedOutError
 }
 
-var invalidConfigError = microerror.New("invalid config")
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
 
 // IsInvalidConfig asserts invalidConfigError.
 func IsInvalidConfig(err error) bool {
 	return microerror.Cause(err) == invalidConfigError
 }
 
-var invalidEventError = microerror.New("invalid event")
+var invalidEventError = &microerror.Error{
+	Kind: "invalidEventError",
+}
 
 // IsInvalidEvent asserts invalidEventError.
 func IsInvalidEvent(err error) bool {
